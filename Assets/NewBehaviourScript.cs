4cffdffde5fc7645d533272945a39c9e0dd3ff57
Martin Chovanec <martin.chovanec@siemens.com>
2018-04-29 19:58:42 +0200

    First commit: crazy cube fountain

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    //public makes it visible for the inspector
+    public GameObject cube;
+	
+    // Use this for initialization
+	void Start () {
+       
+	}
+
+    void Update () {
+		if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(cube, new Vector3(), Quaternion.identity);
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
